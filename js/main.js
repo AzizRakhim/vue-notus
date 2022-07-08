bfdbc7de3534bd97b12628ee601c7771a44368d2
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-07-08 18:57:46 +0500

    finished all sections

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let elSections = document.querySelectorAll(".sections");
+let elSectionBtns = document.querySelectorAll(".sectionBtn");
+
+elSections[0].style.display = "block";
+elSectionBtns[0].classList.add("nav-bar__btn--active");
+
+elSectionBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    elSectionBtns.forEach((item, i) => {
+      item.classList.remove("nav-bar__btn--active");
+      elSections[i].style.display = "none";
+    })
+    btn.classList.add("nav-bar__btn--active");
+    elSections[index].style.display = "block";
+  })
+})
